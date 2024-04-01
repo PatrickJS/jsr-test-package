ccf890ca836b554e2107b3bc1dc565cac96a7454
PatrickJS <github@patrickjs.com>
2024-03-31 18:17:19 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
-export default function main() {
-  console.log("jsr-test-package!");
+export default function main(msg) {
+  const message = "jsr-test-package!" + msg;
+  console.log(message);
+  return message;
 }
